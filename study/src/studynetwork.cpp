9262ee3ad09c47f2bcd01b781d2f4bb658bba228
robin luo <luo_jianguo@163.com>
2013-12-12 01:21:52 +0800

    test submit

--- study/src/studynetwork.cpp
+++ study/src/studynetwork.cpp
@@ -2,9 +2,9 @@
 #include "zombiestudy.h"
 int main()
 {
-	//socketstudy sock;
-	//sock.startdaemon();
-	zombiestudy zombie;
-	zombie.start();
+	socketstudy sock;
+	sock.startdaemon();
+	//zombiestudy zombie;
+	//zombie.start();
     return 0;
 }
